f0b42cab67e843f49edace87f1de9a21036106ae
David Christ <davidckss@proton.me>
2025-05-23 22:17:52 +0900

    use useUrlParameter

--- src/lib/__pages__/login.tsx
+++ src/lib/__pages__/login.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { LoginForm } from "@/components/login-form";
-import { ComponentProps, ReactNode, useEffect, useState } from "react";
+import { ComponentProps, ReactNode } from "react";
 import { Supabase } from "../__supabase__/supabase";
 import { cn } from "../utils";
 import useUrlParameter from "../__hooks__/useUrlParameter";
-import { useSearchParams } from "next/navigation";
 
 /**
  * LoginPage component provides a styled login interface that wraps the `LoginForm` component.
@@ -52,14 +51,7 @@ export default function LoginPage({
   header?: ReactNode;
   footer?: ReactNode;
 }) {
-  const searchParams = useSearchParams();
-  const [defaultEmail, setEmail] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    const param = searchParams.get("email");
-    console.log("searchParams.get('email') →", param);
-    setEmail(param ?? undefined);
-  }, [searchParams]);
+  const defaultEmail = useUrlParameter("email");
 
   return (
     <>
